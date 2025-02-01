d0ab56fde1469792bfb8b475af943fac4a194825
firzalay <firza7004@gmail.com>
2025-02-01 23:04:26 +0700

    add solved memoize problems

--- memoize.js
+++ memoize.js
@@ -3,17 +3,30 @@
  * @return {Function}
  */
 function memoize(fn) {
-    return function (...args) { 
-        fn(...args);
+    const cache = new Map();
+
+    return function (...args) {
         
+        const key = JSON.stringify(args);
+        
+        if (!cache.has(key)) {
+            return cache.set(key, fn(...args)).get(key)
+        } else {
+            return cache.get(key);
+        }
     };
 }
+
+
 
 let callCount = 0;
 const memoizedFn = memoize(function (a, b) {
     callCount += 1;
     return a + b;
 });
-memoizedFn(2, 3); // 5
-memoizedFn(2, 3); // 5
-console.log(callCount); // 1
+
+console.log(memoizedFn(2, 3));
+console.log(memoizedFn(2, 3));
+console.log(callCount);
+
+// memoizedFn(2, 3);
