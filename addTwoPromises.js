c140c82dbfd6d7af63bd66bda280fe03d5516d0c
firzalay <firza7004@gmail.com>
2025-02-02 20:32:19 +0700

    solved addTwoPromises problems

---
+++ addTwoPromises.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function (promise1, promise2) {
+    promise1.then((value) => {
+        return value;
+    });
+
+    promise2.then((value) => {
+        return value;
+    });
+
+    const data1 = await promise1;
+    const data2 = await promise2;
+
+    return data1 + data2;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
---
+++ playground.js
@@ -0,0 +1,5 @@
+function isPalindrome(word) {
+    for (let i = 0; i < word.length; i++) {
+        const element = word[i];
+    }
+}
