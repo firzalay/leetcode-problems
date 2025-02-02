78e3d825f571318cac9f7ca864c95230c390adb7
firzalay <firza7004@gmail.com>
2025-02-02 20:40:27 +0700

    add another solution in addTwoPromises.js

--- addTwoPromises.js
+++ addTwoPromises.js
@@ -4,18 +4,32 @@
  * @return {Promise}
  */
 var addTwoPromises = async function (promise1, promise2) {
-    promise1.then((value) => {
-        return value;
+    // promise1.then((value) => {
+    //     return value;
+    // });
+
+    // promise2.then((value) => {
+    //     return value;
+    // });
+
+    // const data1 = await promise1;
+    // const data2 = await promise2;
+
+    // return data1 + data2;
+
+    const data = Promise.all([promise1, promise2]).then((values) => {
+        return values;
     });
 
-    promise2.then((value) => {
-        return value;
-    });
-
-    const data1 = await promise1;
-    const data2 = await promise2;
+    const [data1, data2] = await data;
 
     return data1 + data2;
+    
+
+    
+    
+
+    
 };
 
 addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
