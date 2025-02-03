4331c58b52b02cf6cccc17a23c3cb1588ded789f
firzalay <firza7004@gmail.com>
2025-02-03 18:25:10 +0700

    testing for git purposes

--- sleep.js
+++ sleep.js
@@ -3,18 +3,18 @@
  * @return {Promise}
  */
 async function sleep(millis) {
-    let myPromise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Hello")
-        }, millis);
-    });
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello")
+    }, millis);
+  });
 
-    return await myPromise;
+  return await myPromise;
 
 
-    
+
 }
 
 let t = Date.now();
 sleep(500).then(() => console.log(Date.now() - t)); // 100
-    
+
