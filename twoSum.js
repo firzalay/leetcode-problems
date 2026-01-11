33ad6550b4f06ce1b9b692fc2f4a00b39d0a97ca
firzalay <firza7004@gmail.com>
2026-01-12 06:17:09 +0700

    twoSum.js : one pass solution

--- twoSum.js
+++ twoSum.js
@@ -4,13 +4,28 @@
  * @return {number[]}
  */
 
+// var twoSum = function (nums, target) {
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             if (nums[i] + nums[j] == target) {
+//                 return [i, j]
+//             }
+//         }
+//     }
+// };
+
+// Optimal Solution
 var twoSum = function (nums, target) {
+    const numberToIndex = {};
+
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                return [i, j]
-            }
+        const numberNeeded = target - nums[i];
+ 
+        if (numberToIndex[numberNeeded] !== undefined) {
+            return [i, numberToIndex[numberNeeded]];
         }
+
+        numberToIndex[nums[i]] = i;
     }
 };
 
