4e22b383ec236cdc65b947126bef2750dce7b265
firzalay <firza7004@gmail.com>
2026-01-18 23:21:44 +0700

    try using unoptimized solution for containsDuplicate.js

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -1,10 +1,31 @@
 var containsDuplicate = function(nums) {
-    const seen = new Set();
-    for (const num of nums) {
-        if (seen.has(num)) {
-            return true; 
-        }
-        seen.add(num); 
+  const set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+        return true;
     }
-    return false; 
+
+    set.add(nums[i])
+  }
+
+  return false;
 };
+
+
+var containsDuplicateRaw = function(nums) {
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] == nums[j]) {
+          return true
+        }
+    }
+  }
+
+  return false;
+};
+
+
+console.log(containsDuplicate([1,2,3,4]));
+console.log(containsDuplicateRaw([1,2,3,1]));
